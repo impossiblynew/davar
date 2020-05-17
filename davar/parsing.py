914e0119e030e724bc762188f7fb2e7355559c6b
Impossibly New <newlyimpossible@gmail.com>
2020-05-17 16:56:19 -0700

    removed _ before function and class names

--- davar/parsing.py
+++ davar/parsing.py
@@ -4,20 +4,20 @@ from davar import model
 
 # define rules
 # fmt: off
-def _numerical_id(): return _(r'\d+')
+def numerical_id(): return _(r'\d+')
 
-def _q_id(): return "Q", _numerical_id
+def q_id(): return "Q", numerical_id
 
-def _p_id(): return "P", _numerical_id
+def p_id(): return "P", numerical_id
 
-def _statement(): return "(", _p_id, _q_id, _q_id, ")"
+def statement(): return "(", p_id, q_id, q_id, ")"
 
-def _davar(): return OneOrMore(_statement), EOF
+def davar(): return OneOrMore(statement), EOF
 # fmt: on
 
 # visitor class
-class _DavarVisitor(PTNodeVisitor):
-    def visit__numerical_id(self, node, children):
+class DavarVisitor(PTNodeVisitor):
+    def visit_numerical_id(self, node, children):
         """
         Converts numerical_id value to int.
         """
@@ -25,7 +25,7 @@ class _DavarVisitor(PTNodeVisitor):
             print(f"Converting {node.value}.")
         return int(node.value)
 
-    def visit__q_id(self, node, children):
+    def visit_q_id(self, node, children):
         """
         Instantiates a Node for each q_id.
         """
@@ -33,7 +33,7 @@ class _DavarVisitor(PTNodeVisitor):
             print(f"Instantiating Node from {children}.")
         return model.Node(children[0])
 
-    def visit__p_id(self, node, children):
+    def visit_p_id(self, node, children):
         """
         Instantiates a Rel for each p_id.
         """
@@ -41,7 +41,7 @@ class _DavarVisitor(PTNodeVisitor):
             print(f"Instantiating Rel from {children}.")
         return model.Rel(children[0])
 
-    def visit__statement(self, node, children):
+    def visit_statement(self, node, children):
         """
         Instantiates a Statement for each statement.
         """
@@ -49,7 +49,7 @@ class _DavarVisitor(PTNodeVisitor):
             print(f"Instantiating Statement from {children}.")
         return model.LabeledEdge(*children)
 
-    def visit__davar(self, node, children):
+    def visit_davar(self, node, children):
         """
         Collects Statements into a List.
         """
@@ -58,19 +58,19 @@ class _DavarVisitor(PTNodeVisitor):
         return list(children)
 
 
-def _parse(davartext: str, debug: bool = False):
-    parser = ParserPython(_davar, debug=debug)
+def parse(davartext: str, debug: bool = False):
+    parser = ParserPython(davar, debug=debug)
     return parser.parse(davartext)
 
 
-def _visit(davartree, debug: bool = False) -> list:
-    return visit_parse_tree(davartree, _DavarVisitor(debug=debug))
+def visit(davartree, debug: bool = False) -> list:
+    return visit_parse_tree(davartree, DavarVisitor(debug=debug))
 
 
 def transcribe(davartext: str, debug: bool = False) -> list:
     """
     Parses a text string in davartext into a list of Statements.
     """
-    parse_tree = _parse(davartext, debug=debug)
-    result = _visit(parse_tree, debug=debug)
+    parse_tree = parse(davartext, debug=debug)
+    result = visit(parse_tree, debug=debug)
     return result
