6b0e1c3af788b030e21b7ed500de27cb8c4d493a
Impossibly New <newlyimpossible@gmail.com>
2020-05-17 18:27:37 -0700

    renamed tests to be consistant in style removed the name of the class it is testing from tests under classes, like had already been done with the Node class.

--- tests/test_model.py
+++ tests/test_model.py
@@ -38,55 +38,53 @@ class TestNode:
 
 
 class TestRel:
-    def test_Rel_describe(self, cached_rel_P31):
+    def test_describe(self, cached_rel_P31):
         assert cached_rel_P31.describe("en") == "instance of"
 
-    def test_Rel_repr(self, cached_rel_P31):
+    def test_repr(self, cached_rel_P31):
         assert repr(cached_rel_P31) == "Rel(31)"
 
-    def test_Rel_str(self, cached_rel_P31):
+    def test_str(self, cached_rel_P31):
         assert str(cached_rel_P31) == "P31"
 
 
 class TestStatement:
-    def test_singletonStatement_repr(self, cached_node_Q42):
+    def test_repr(self, cached_node_Q42):
         s = model.Statement(cached_node_Q42)
         assert repr(s) == "Statement(Node(42))"
 
-    def test_singletonStatement_str(self, cached_node_Q42):
+    def test_str(self, cached_node_Q42):
         s = model.Statement(cached_node_Q42)
         assert str(s) == "(Q42)"
 
-    def test_singletonStatement_describe(self, cached_node_Q42):
+    def test_describe(self, cached_node_Q42):
         s = model.Statement(cached_node_Q42)
         assert s.describe("en") == "Douglas Adams."
 
 
 class TestEdge:
-    def test_Edge_repr(self, cached_node_Q42, cached_node_Q5):
+    def test_repr(self, cached_node_Q42, cached_node_Q5):
         s = model.Edge(cached_node_Q42, cached_node_Q5)
         assert repr(s) == "Edge(Node(42), Node(5))"
 
-    def test_Edge_str(self, cached_node_Q42, cached_node_Q5):
+    def test_str(self, cached_node_Q42, cached_node_Q5):
         s = model.Edge(cached_node_Q42, cached_node_Q5)
         assert str(s) == "(Q42 Q5)"
 
-    def test_Edge_describe(self, cached_node_Q42, cached_node_Q5):
+    def test_describe(self, cached_node_Q42, cached_node_Q5):
         s = model.Edge(cached_node_Q42, cached_node_Q5)
         assert s.describe("en") == "Douglas Adams -> human."
 
 
 class TestLabeledEdge:
-    def test_LabeledEdge_repr(self, cached_rel_P31, cached_node_Q42, cached_node_Q5):
+    def test_repr(self, cached_rel_P31, cached_node_Q42, cached_node_Q5):
         s = model.LabeledEdge(cached_rel_P31, cached_node_Q42, cached_node_Q5)
         assert repr(s) == "LabeledEdge(Rel(31), Node(42), Node(5))"
 
-    def test_LabeledEdge_str(self, cached_rel_P31, cached_node_Q42, cached_node_Q5):
+    def test_str(self, cached_rel_P31, cached_node_Q42, cached_node_Q5):
         s = model.LabeledEdge(cached_rel_P31, cached_node_Q42, cached_node_Q5)
         assert str(s) == "(P31 Q42 Q5)"
 
-    def test_LabeledEdge_describe(
-        self, cached_rel_P31, cached_node_Q42, cached_node_Q5
-    ):
+    def test_describe(self, cached_rel_P31, cached_node_Q42, cached_node_Q5):
         s = model.LabeledEdge(cached_rel_P31, cached_node_Q42, cached_node_Q5)
         assert s.describe("en") == "Douglas Adams -> human (instance of)."
