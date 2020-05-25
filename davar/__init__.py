ae859e8a038c8872b7be75f9b95b707d6cb090eb
Impossibly New <newlyimpossible@gmail.com>
2020-05-25 12:27:49 -0700

    add OMWSynset to parser + fix data download code - now also installs wordnet - now adds local data to nltk path

--- davar/__init__.py
+++ davar/__init__.py
@@ -9,5 +9,12 @@ try:
 except LookupError:
     nltk.download("omw", r"./nltk_data")
 
+try:
+    nltk.data.find(r"corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet", r"./nltk_data")
+
+nltk.data.path.append(r"./nltk_data")
+
 __version__ = "0.1.1"
 name = "davar"
