ea4354eb976a3d3f3fe5c258cfc38fd61995049b
Impossibly New <newlyimpossible@gmail.com>
2020-05-26 20:53:33 -0700

    add napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = "0.2.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["autoapi.extension"]
+extensions = ["autoapi.extension", "sphinx.ext.napoleon"]
 autoapi_dirs = ["../davar"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
