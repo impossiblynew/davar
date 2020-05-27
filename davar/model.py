695ba023e64d82f130a449f8186e30f1f1adc862
Impossibly New <newlyimpossible@gmail.com>
2020-05-26 17:34:54 -0700

    add docstrings to model.py

--- davar/model.py
+++ davar/model.py
@@ -5,44 +5,80 @@ import pycountry
 
 
 class DavarWord:
-    """
-    Informal abstract class for Nodes and Rels. Implements some methods but will throw a 
-    NotImplementedError if .describe() is used.
+    """Informal abstract class for Words, like Rel and Node.
     """
 
     def __init__(self, id: str):
+        """Initializes a Word from an identifier. This should only be used on 
+        super().__init__.
+
+        Parameters
+        ----------
+        id : str
+            Word identifier
+        """
         self._validate_id(id)
         self.id = id
 
-    def _validate_id(self, id):  # Not sure if this should be a static or class method
-        """
-        Informal abstract method for validating id at init. Raises error if the id
-        is not valid.
+    def _validate_id(self, id: str):
+        """Informal abstract method for validating id on init. This method should be
+        implemented by any subclass that will be initialized. When implemented, it
+        should do nothing if the id is valid and throw a ValueError if the id is invalid
+        .
+
+        Parameters
+        ----------
+        id : str
+            Word identifier
+        Raises
+        ------
+        NotImplementedError
+            This will always be raised if this method is called, as it is an abstract
+            method.
         """
         raise NotImplementedError
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Returns representation of self"""
         return f'{type(self).__name__}("{self.id}")'
 
-    def __str__(self):
-        return self.id
+    def __str__(self) -> str:
+        """Returns pretty printed representation of self"""
+        return str(self.id)
 
     def __eq__(self, other) -> bool:
+        """Returns True if equal, false if not."""
         return self.id == other.id
 
     def describe(self, lang: str, lvl: int = 0) -> str:
-        """
-        Informal abstract method. This should describe itself in the given language,
-        modulating output according to lvl.
+        """Informal abstract method. Should be implemented by any subclasses. When
+        implemented, returns a human-readable description of the Word in a given 
+        language depending on level of hierachy.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            Hierachy level of description in output text, by default 0
+
+        Returns
+        -------
+        str
+            Description of self
+
+        Raises
+        ------
+        NotImplementedError
+            Will always be raised if this method as called, as it is an abstract method.
         """
         raise NotImplementedError
 
 
 class Node(DavarWord):
-    """
-    Empty class for Nodes.
-    Nodes and Rels share a lot of code. This class exists so that subclasses of Node and
-    subclasses of Rel can be distinguished.
+    """Informal abstract class for Nodes in davar, which represent nouns, ideas, or 
+    entities. Is empty because all code is shared with Rel, and exists so that
+    subclasses of Node and of Rel can be distinguished easily.
     """
 
     def __init__(self, id):
@@ -50,10 +86,9 @@ class Node(DavarWord):
 
 
 class Rel(DavarWord):
-    """
-    Empty class for Nodes.
-    Nodes and Rels share a lot of code. This class exists so that subclasses of Node and
-    subclasses of Rel can be distinguished.
+    """Informal abstract class for Rel in davar, which represent properties, verbs, or any 
+    other type of relationship between Nodes. Is empty because all code is shared with 
+    Node, and exists so that subclasses of Node and of Rel can be distinguished easily.
     """
 
     def __init__(self, id):
@@ -61,81 +96,158 @@ class Rel(DavarWord):
 
 
 class WikidataItem(Node):
-    """
-    Class for WikidataItems, a kind of node. Initialize with a string giving the
-    Wikidata Item Identifier. Calling describe will give the label in the requested
-    language.
+    """Class for Wikidata Items, a type of Node, which is itself a type of DavarWord.
     """
 
     _compiled_id_regex = compile(r"Q\d+")
 
     def __init__(self, id: str):
+        """Constructs and initializes a WikidataItem from a valid Wikidata Property
+        identifier.
+
+        Parameters
+        ----------
+        id : str
+            A valid Wikidata Item identifier in the form Q# where # is a natural number.
+        """
         super().__init__(id)
         self.data = Client().get(id)
 
     @classmethod
     def _validate_id(cls, id: str):
+        """Called at `__init__` to validate identifiers, which should be in the form
+        `Q#` where `#` is a natural number. Does nothing if the identifier is valid, but
+        throws a ValueError if it is invalid.
+
+        Parameters
+        ----------
+        id : str
+            Hopefully, a valid Wikidata Item identifier.
+
+        Raises
+        ------
+        ValueError
+            Thrown if `id` is not, in fact, a valid Wikidata Item identifier.
+        """
         if cls._compiled_id_regex.fullmatch(id) == None:
             raise ValueError(f"{id} is not a valid Wikidata Item ID")
 
     def describe(self, lang: str, lvl: int = 0) -> str:
-        """
-        Finds the label of a WikiData item given a two letter language code.
+        """Returns the Wikidata Item label in a given language. Ignores hierarchy, as
+        Wikidata Items are displayed the same regardless of hierarchy.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            Hierachy level of description in output text, by default 0
+
+        Returns
+        -------
+        str
+            Wikidata Item label in given language
         """
         return self.data.label[lang]
 
 
 class WikidataProperty(Rel):
-    """
-    A relationship between nodes. Right now is simply a container for Wikidata property
-    IDs.
+    """Class for Wikidata Properties, a type of Rel, which is itself a type of DavarWord.
     """
 
     _compiled_id_regex = compile(r"P\d+")
 
     def __init__(self, id: str):
+        """Constructs and initializes a WikidataProperty from a valid Wikidata
+        Property identifier.
+
+        Parameters
+        ----------
+        id : str
+            A valid Wikidata Property identifier in the form `P#` where `#` is a
+            natural number.
+        """
         super().__init__(id)
         self.data = Client().get(id)
 
     @classmethod
     def _validate_id(cls, id):
+        """Called at `__init__` to validate identifiers, which should be in the form
+        `P#` where `#` is a natural number. Does nothing if the identifier is valid, but
+        throws a ValueError if it is invalid.
+
+        Parameters
+        ----------
+        id : str
+            Hopefully, a valid Wikidata Property identifier.
+
+        Raises
+        ------
+        ValueError
+            Thrown if `id` is not, in fact, a valid Wikidata Property identifier.
+        """
         if cls._compiled_id_regex.fullmatch(id) == None:
             raise ValueError(f"{id} is not a valid Wikidata Property ID")
 
     def describe(self, lang: str, lvl: int = 0) -> str:
+        """Returns the Wikidata Property label in a given language. Ignores hierarchy,
+        as Wikidata Properties are displayed the same regardless of hierarchy.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            Hierachy level of description in output text, by default 0
+
+        Returns
+        -------
+        str
+            Wikidata Property label in given language
         """
-        Finds the label of a WikiData item given a two letter language code.
-        """
-        return self.data.label[lang]  # returns same regardless of level
+        return self.data.label[lang]
 
 
 class OMWSynset(Node, Rel):
-    """
-    A synset from the open multilingual wordnet.
+    """A Synonym Set in the Open Multilingual Wordnet, representing a set of synonyms
+    that represent the same idea across languages.
     """
 
     _compiled_id_regex = compile(r"\d{8}-[v|r|n|a]")
 
     def __init__(self, id: str):
+        """Constructs a OMWSynset from an "identifier," more formally Part Of Speech and
+        offset, in the form `OFFSET-POS` where `OFFSET` is an eight digit code and POS
+        is a letter representing part of speech, either 'v,' 'r,' 'n,' or 'a.'
+
+        Parameters
+        ----------
+        id : str
+            Synset offset and POS.
+        """
         super().__init__(id)
 
     @classmethod
     def _validate_id(cls, id):
         if cls._compiled_id_regex.fullmatch(id) == None:
             raise ValueError(
-                f"{id} is not a valid Open Multilingual WordNet synset offset"
+                f"{id} is not a valid Open Multilingual WordNet Synset offset and POS"
             )
 
     @staticmethod
     def _bcp_47_to_iso_639_2(lang_tag: str) -> str:
-        """Utility function for turning BCP 47 language tags into ISO 639-2 language
-        tags.
+        """Utility function for getting a ISO 639-2 three letter language code from a 
+        BCP 47 language tag.
 
-        Arguments:
-            lang_tag {str} -- Langauge tag/code in BCP 47
+        Parameters
+        ----------
+        lang_tag : str
+            BCP 47 language tag/code.
 
-        Returns:
-            str -- Rougly corresponding language code in ISO 639-2
+        Returns
+        -------
+        str
+            Roughly equivalent ISO 639-2 three letter language code.
         """
 
         if "-" in lang_tag:
@@ -146,11 +258,21 @@ class OMWSynset(Node, Rel):
         else:
             return pycountry.languages.get(alpha_2=lang_tag).alpha_3
 
-    def describe(
-        self, lang: str, lvl: int = 0
-    ) -> str:  # FIXME: It is impossible to use this together with Wikidata items because they use a different language scheme.
-        """
-        Gives the lemma (name) of a Open Multilingual Wordnet synset in a language given in a 3 letter code 
+    def describe(self, lang: str, lvl: int = 0) -> str:
+        """Returns the first listed lemma name for Synset in a given language. Ignores
+        hierarchy as Synsets do not change display depending on hierarchy.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            Hierachy level of description in output text, by default 0
+
+        Returns
+        -------
+        str
+            First lemma name for Synset in a given language.
         """
         return wn.synset_from_pos_and_offset(
             self.id[-1], int(self.id[:-2])
@@ -158,11 +280,18 @@ class OMWSynset(Node, Rel):
 
 
 class Statement:
-    """
-    Most basic possible statement, which involves only a subject.
+    """Most basic form of Statement (often refered to as a Singleton Statement)
+    involving only a subject.
     """
 
     def __init__(self, sub):
+        """Constructs a Singleton Statement from a subject.
+
+        Parameters
+        ----------
+        sub : Node or Statement
+            The subject of this statement
+        """
         self.sub = sub
 
     def __eq__(self, other) -> bool:
@@ -175,9 +304,22 @@ class Statement:
         return f"({self.sub})"
 
     def describe(self, lang: str, lvl: int = 0) -> str:
-        """
-        Translates / describes self in language as given in two character language code
-        form in `lang`
+        """Describes self in human readable format in a given language by calling
+        `.describe()` on children and structuring results in a human readable format.
+        Will return slightly different formatting to minimize confusion if lvl is
+        greater than 0.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            The level of hierarchy in the text description, by default 0
+
+        Returns
+        -------
+        str
+            Description of self in given language.
         """
         sub_label = self.sub.describe(lang, lvl + 1)
         if lvl == 0:  # give fancy formatting if it is top level
@@ -187,11 +329,19 @@ class Statement:
 
 
 class Edge(Statement):
-    """
-    Defines a relation between a subject and an object.
+    """Statement defining an unlabeled relationship between a subject and an object.
     """
 
     def __init__(self, sub, ob):
+        """Constructs an edge from the subject to the object.
+
+        Parameters
+        ----------
+        sub : Node or Statement
+            Subject / origin of Edge
+        ob : Node or Statement
+            Object / endpoint of Edge
+        """
         self.ob = ob
         super().__init__(sub)
 
@@ -202,9 +352,22 @@ class Edge(Statement):
         return f"({self.sub} {self.ob})"
 
     def describe(self, lang: str, lvl: int = 0) -> str:
-        """
-        Translates / describes self in language as given in two character language code
-        form in `lang`
+        """Describes self in human readable format in a given language by calling
+        `.describe()` on children and structuring results in a human readable format.
+        Will return slightly different formatting to minimize confusion if lvl is
+        greater than 0.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            The level of hierarchy in the text description, by default 0
+
+        Returns
+        -------
+        str
+            Description of self in given language.
         """
         sub_label = self.sub.describe(lang, lvl + 1)
         ob_label = self.ob.describe(lang, lvl + 1)
@@ -215,12 +378,21 @@ class Edge(Statement):
 
 
 class LabeledEdge(Edge):
-    """
-    Defines a relationship between a subject Node and an object Node which is labeled
-    with a Rel.
+    """Statement defining a labeled relationship from a subject to an object.
     """
 
     def __init__(self, rel: Rel, sub, ob):
+        """Constructs a LabeledEdge from subject to object, labeled by a rel.
+
+        Parameters
+        ----------
+        rel : Rel
+            Relationship between subject and object
+        sub : Node or Statement
+            Subject / origin of LabeledEdge
+        ob : Node or Statement
+            Object / endpoint of LabeledEdge
+        """
         self.rel = rel
         super().__init__(sub, ob)
 
@@ -231,9 +403,22 @@ class LabeledEdge(Edge):
         return f"({self.rel} {self.sub} {self.ob})"
 
     def describe(self, lang: str, lvl: int = 0) -> str:
-        """
-        Translates / describes self in language as given in two character language code
-        form in `lang`
+        """Describes self in human readable format in a given language by calling
+        `.describe()` on children and structuring results in a human readable format.
+        Will return slightly different formatting to minimize confusion if lvl is
+        greater than 0.
+
+        Parameters
+        ----------
+        lang : str
+            BCP 47 language tag
+        lvl : int, optional
+            The level of hierarchy in the text description, by default 0
+
+        Returns
+        -------
+        str
+            Description of self in given language.
         """
         rel_label = self.rel.describe(lang, lvl + 1)
         sub_label = self.sub.describe(lang, lvl + 1)
@@ -247,12 +432,16 @@ class LabeledEdge(Edge):
 
 def _bcp_47_to_iso_639_2(lang_code: str) -> str:
     """For backwards compatibility with 0.2.0, mirrors staticmethod of OMWSynset's
-    ._bcp_42_to_iso_639_2() method
+    `._bcp_42_to_iso_639_2()` method
 
-    Arguments:
-        lang_code {str} -- Lang tag/code in BCP 47
+    Parameters
+    ----------
+    lang_code : str
+        Language tag/code in BCP 47
 
-    Returns:
-        str -- Lang tag/code in ISO 639-2
+    Returns
+    -------
+    str
+        Language tag/code in ISO 639-2
     """
     return OMWSynset._bcp_47_to_iso_639_2(lang_code)
